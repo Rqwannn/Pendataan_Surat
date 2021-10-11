4aa3a97a9e62ecb80f99c726ee77f6ace4fe1341
Rqwannn <mohwannn1@gmail.com>
2021-10-11 13:35:42 +0700

    update

--- resources/js/page/Home.vue
+++ resources/js/page/Home.vue
@@ -1,10 +1,6 @@
 <template>
     <div>
-        <p>
-            Tanyakan pertanyaan iya/tidak:
-            <input v-model="question">
-        </p>
-        <p>{{ answer }}</p>
+
     </div>
 </template>
 
@@ -12,9 +8,8 @@
     export default{
         data () {
             return {
-                question: '',
-                answer: 'Saya tidak dapat memberikan jawaban sampai anda bertanya kepada saya!',
-                Title : "Home"
+                Title : "Home",
+                DataSurat : []
             }
         },
         watch: {
@@ -24,30 +19,11 @@
                     document.title = this.Title;
                 }
             },
-            question: function (newQuestion, oldQuestion) {
-                this.answer = 'Waiting for you to stop typing...'
-                this.GetAnswer()
-            }
         },
-        created: function () {
-            this.GetAnswer = _.debounce(this.getAnswer, 500)
+        mounted(){
+            this.getSurat();
         },
         methods: {
-            getAnswer:  function () {
-            if (this.question.indexOf('?') === -1) {
-                this.answer = 'Pertanyaan biasanya berisi tanda tanya. ;-)'
-                return
-            }
-            this.answer = 'Berfikir...'
-            var vm = this
-            axios.get('https://yesno.wtf/api')
-                .then(function (response) {
-                vm.answer = _.capitalize(response.data.answer)
-                })
-                .catch(function (error) {
-                vm.answer = 'Error! Tidak dapat meraih API. ' + error
-                })
-            },
         }
     }
 </script>
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register web routes for your application. These-
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
@@ -19,4 +19,4 @@ Route::get('/', function () {
 
 Route::get('/{any}', function () {
     return view('index');
-})->where('any', '.*'); // agar membaca route milik vue js
+})->where('any', '.*');
