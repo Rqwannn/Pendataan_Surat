e82e1497d97dc51ac3c918908828c5c8a3e79821
Rizki Ramadhan <farazrizki13@gmail.com>
2021-10-13 21:51:13 +0700

    finish my part, it's all yours. byee

--- app/Http/Controllers/OutgoingMailController.php
+++ app/Http/Controllers/OutgoingMailController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ValidationIncomingMails;
+use App\Http\Requests\ValidationIncomingMailsUpdate;
 use App\Models\OutgoingMail;
-use Illuminate\Http\Request;
 
 class OutgoingMailController extends Controller
 {
@@ -41,7 +41,7 @@ class OutgoingMailController extends Controller
             $file_ext = $request->file('file')->extension(); // Get an extension of file.
 
             $destination_path = './files/'; // Define path.
-            $file = 'incoming_mail-' . time() . '.' . $file_ext; // Make random new name of file.
+            $file = 'outgoing_mail-' . time() . '.' . $file_ext; // Make random new name of file.
 
 
             /*
@@ -59,7 +59,13 @@ class OutgoingMailController extends Controller
 
             $file_name = $file; // set file name
         }
-        
+
+        /*
+            |--------------------------------------------------------------------------
+            | Save file to database.
+            |--------------------------------------------------------------------------
+        */
+
         $mail = OutgoingMail::create([
             'title' => $request->title,
             'subject' => $request->subject,
@@ -67,6 +73,7 @@ class OutgoingMailController extends Controller
             'to' => $request->to,
             'phone' => $request->phone,
             'content' => $request->content,
+            "file" => $file_name
         ]);
 
         $status = true;
@@ -102,9 +109,16 @@ class OutgoingMailController extends Controller
     }
 
 
-    public function update(ValidationIncomingMails $request, $id)
+    public function update(ValidationIncomingMailsUpdate $request, $id)
     {
+        $destination_path = './files/'; // Define path.
         $outgoingMail = OutgoingMail::where("id", $id)->first();
+
+        /*
+        |--------------------------------------------------------------------------
+        | Check if the mail was there.
+        |--------------------------------------------------------------------------
+        */
 
         if (!$outgoingMail) {
             return response()->json([
@@ -112,6 +126,53 @@ class OutgoingMailController extends Controller
                 'message' => "Wrong id, please check your id."
             ], 200);
         }
+
+        /*
+        |--------------------------------------------------------------------------
+        | Check if users upload file or not. Then get pdf uploaded file.
+        |--------------------------------------------------------------------------
+        */
+
+        $file_name = $request->old_file; // if user not upload this will fill by old file name
+
+        if ($request->hasFile('file')) {
+            $file_ext = $request->file('file')->extension(); // Get an extension of file.
+
+            $file = 'outgoing_mail-' . time() . '.' . $file_ext; // Make random new name of file.
+
+
+            /*
+            |--------------------------------------------------------------------------
+            | Move the file to folder on server.
+            |--------------------------------------------------------------------------
+            */
+
+            if (!$request->file('file')->move($destination_path, $file)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => "Cannot upload file."
+                ], 200);
+            }
+
+
+            /*
+            |--------------------------------------------------------------------------
+            | Delete the older file, cause it's not necessary anymore.
+            |--------------------------------------------------------------------------
+            */
+
+            if (file_exists($destination_path . $file_name)) {
+                unlink($destination_path . $file_name); // remove file
+            }
+
+            $file_name = $file; // set new file name
+        }
+
+        /*
+        |--------------------------------------------------------------------------
+        | Save new data to database.
+        |--------------------------------------------------------------------------
+        */
 
         $mail = OutgoingMail::where("id", $id)->update([
             'title' => $request->title,
@@ -140,13 +201,30 @@ class OutgoingMailController extends Controller
 
     public function destroy($id)
     {
+        $destination_path = './files/'; // Define path.
         $outgoingMail = OutgoingMail::where("id", $id)->first();
+
+        /*
+            |--------------------------------------------------------------------------
+            | Check if the mail was there.
+            |--------------------------------------------------------------------------
+        */
 
         if (!$outgoingMail) {
             return response()->json([
                 'success' => false,
                 'message' => "Wrong id, please check your id."
             ], 200);
+        }
+
+        /*
+            |--------------------------------------------------------------------------
+            | Delete file, cause it's not necessary anymore.
+            |--------------------------------------------------------------------------
+        */
+
+        if (file_exists($destination_path . $outgoingMail->file)) {
+            unlink($destination_path . $outgoingMail->file); // remove file
         }
 
         $delete = OutgoingMail::where('id', $id)->delete();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        /*
+        |--------------------------------------------------------------------------
+        | Seeder For table users.
+        |--------------------------------------------------------------------------
+        */
+
         DB::table('users')->insert([
             'name' => "jelita rahma",
             'username' => "jelita",
             'password' => bcrypt('jelita123'),
             'role' => 1
+        ]);
+
+        /*
+        |--------------------------------------------------------------------------
+        | Seeder For table outgoing_mails.
+        |--------------------------------------------------------------------------
+        */
+        DB::table('incoming_mails')->insert([
+            'title' => "Frozen Thunder",
+            'subject' => "Frozen Thunder Movie",
+            'from' => "Prince Hans",
+            'to' => "Queen Anna",
+            'phone' => "081394524240",
+            'content' => "Let's go for Frozen Thunder Movie tommorow anna",
+            "file" => "incoming_mail-1634136509.pdf"
+        ]);
+
+        /*
+        |--------------------------------------------------------------------------
+        | Seeder For table outgoing_mails.
+        |--------------------------------------------------------------------------
+        */
+        DB::table('outgoing_mails')->insert([
+            'title' => "Anna Coronation days",
+            'subject' => "Queen Anna Coronation Day",
+            'from' => "Ricky the thunder",
+            'to' => "Elsa of arrandlee",
+            'phone' => "081394524240",
+            'content' => "Wanna come with me to anna coronation tomorow",
+            "file" => "outgoing_mail-1634135812.pdf"
         ]);
     }
 }
